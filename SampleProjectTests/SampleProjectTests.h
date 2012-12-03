f61e50e50f903cbde797297095c400ac0d8ada73
sassembla <sassembla@hotmail.com>
2012-12-04 02:40:50 +0900

    Initial Commit

---
+++ SampleProjectTests/SampleProjectTests.h
@@ -0,0 +1,13 @@
+//
+//  SampleProjectTests.h
+//  SampleProjectTests
+//
+//  Created by Toru Inoue on 2012/12/04.
+//  Copyright (c) 2012年 Toru Inoue. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SampleProjectTests : SenTestCase
+
+@end
---
+++ SampleProjectTests/SampleProjectTests.m
@@ -0,0 +1,32 @@
+//
+//  SampleProjectTests.m
+//  SampleProjectTests
+//
+//  Created by Toru Inoue on 2012/12/04.
+//  Copyright (c) 2012年 Toru Inoue. All rights reserved.
+//
+
+#import "SampleProjectTests.h"
+
+@implementation SampleProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SampleProjectTests");
+}
+
+@end
